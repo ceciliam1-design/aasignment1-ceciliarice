0315d071bc86953f9f09ee38b461bf5c65297aef
ceciliam1-design <ccricerocks1@gmail.com>
2026-04-19 14:08:30 -0400

    Add dark mode toggle functionality

--- .js
+++ .js
@@ -16,3 +16,15 @@ navLinks.forEach(link => {
         link.classList.remove('active-link');
     });
 });
+const toggleBtn = document.getElementById('toggle-mode');
+
+toggleBtn.addEventListener('click', () => {
+    
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) {
+        toggleBtn.textContent = 'Switch to Normal Mode';
+    } else {
+        toggleBtn.textContent = 'Switch to Low Exposure';
+    }
+});
